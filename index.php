cbfca2f84913344064da4326e21f92208e844c82
gjhong <86422308+gjhong@users.noreply.github.com>
2021-12-21 17:06:48 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>manga RDR</title>
+</head> 
+<link rel="stylesheet" href="b1.css">
+
+<body>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+  gtag('config', 'UA-134735971-3');
+</script>
+<ul>
+
+    <li type="h2"><a href="#">link 1</a></li>
+    <li><a href="#">link 2</a></li>
+    <li><a href="#">link 3</a></li>
+    <li><a href="#">link 4</a></li>
+    <li><a href="#">link 5</a></li>
+</ul>
+</body>
+</html>
